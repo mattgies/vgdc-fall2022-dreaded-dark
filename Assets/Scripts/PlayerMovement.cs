f627d206ff793bbba0590dcd661b0a05a96d71c7
Edwin Ma <awesomeedwinma@gmail.com>
2022-11-01 00:16:19 -0700

    Implemented double jump
    W key and UpArrow key can also trigger jump now.
    Double jump enabled where it is reset upon touching the ground or anything tagged as ground.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     
     private Rigidbody2D rb;
+    private int jumps;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumps = 2;
     }
 
     // Update is called once per frame
@@ -18,9 +20,15 @@ public class PlayerMovement : MonoBehaviour
     {
         float dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 4f, rb.velocity.y);
-
-        if (Input.GetButtonDown("Jump")) {
+        bool conditionForJump = Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
+        if ((conditionForJump) && jumps > 0) {
             rb.velocity = new Vector2(rb.velocity.x, 6f);
+            jumps--;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.tag == "Ground")
+            jumps = 2;
+
     }
 }
