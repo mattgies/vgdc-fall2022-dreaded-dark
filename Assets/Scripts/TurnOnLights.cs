efeb4142f4b873255d80c2ed4df3b7ed71b8b685
Luis Escobar <luisaeniebla@gmail.com>
2022-11-04 20:24:11 -0700

    Added a timer to light switches
    Added a timer to light switches which is about 10 seconds right now.

--- Assets/Scripts/TurnOnLights.cs
+++ Assets/Scripts/TurnOnLights.cs
@@ -7,6 +7,8 @@ public class TurnOnLights : MonoBehaviour
 {
     [SerializeField] GameObject globalLight;
     private UnityEngine.Rendering.Universal.Light2D light;
+    private bool isTimerRunning = false;
+    private float timeRemaining = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,19 @@ public class TurnOnLights : MonoBehaviour
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other) {
         light.intensity = 1.0f;
+        //start the timer when you hit the switch
+        isTimerRunning = true;
+        timeRemaining = 10.0f;
+    }
+
+    void Update(){
+        if(isTimerRunning){
+            timeRemaining -= Time.deltaTime;
+
+            if(timeRemaining < 0.0f){
+                isTimerRunning = false;
+                light.intensity = 0.2f;
+            }
+        }
     }
 }
