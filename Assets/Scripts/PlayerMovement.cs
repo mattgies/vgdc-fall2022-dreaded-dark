def25e23136c393a9df27c6b3e90c176a7e05865
amsicat3025 <asicat@uci.edu>
2022-11-06 09:17:43 -0800

    Added dashing to the PlayerMovement scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,16 @@ public class PlayerMovement : MonoBehaviour
     private int jumps;
     private bool _canMove = true;
 
+    // Code for dashing
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
+   // [SerializeField] private TrailRenderer tr;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +28,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isDashing)
+        {
+            return;
+        }
+
         if (_canMove) {
             float dirX = Input.GetAxisRaw("Horizontal");
             rb.velocity = new Vector2(dirX * 4f, rb.velocity.y);
@@ -29,6 +44,11 @@ public class PlayerMovement : MonoBehaviour
             }
         } else{
             rb.velocity = new Vector2(0, 0);
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
         }
     }
     private void OnCollisionEnter2D(Collision2D collision){
@@ -42,5 +62,23 @@ public class PlayerMovement : MonoBehaviour
 
     public void enableMovement() {
         _canMove = true;
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        //tr.emitting = true;
+
+        //Stops dashing
+        yield return new WaitForSeconds(dashingTime);
+        //tr.emitting = false; 
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
