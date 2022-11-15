5c202a8a1aeca3c88e1d85d6cfefdeab6df164fe
Edwin Ma <awesomeedwinma@gmail.com>
2022-11-15 15:10:21 -0800

    Camera lag on death
    Camera will now stay at y=-10 whenever the player falls below the y=-10 height. Since the lowest platform stands at y=-8, being at y=-10 in the first place implies death.

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -7,6 +7,15 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] GameObject thingToFollow;
     void LateUpdate()
     {
-        transform.position = thingToFollow.transform.position + new Vector3(0, 0, -10);        
+        //camera will lag in the air on death
+        if (thingToFollow.transform.position.y < -10){
+            transform.position = new Vector3(thingToFollow.transform.position.x, -10, -10);
+        }
+        //otherwise it follows the player
+        else{
+            transform.position = thingToFollow.transform.position + new Vector3(0, 0, -10);   
+        }     
     }
+
+
 }
