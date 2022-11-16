ea06a4002e45bc15369317d7d4c152415b2c204e
Edwin Ma <awesomeedwinma@gmail.com>
2022-11-15 16:16:10 -0800

    Added a total deaths count
    No text implemented

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -6,6 +6,7 @@ public class PlayerDeath : MonoBehaviour
 { 
     private Checkpoint checkpointPos;
     private TrailRenderer playerTrail;
+    private int totalDeaths = 0;
 
     private void Start(){
         checkpointPos = GetComponent<Checkpoint>();
@@ -13,12 +14,13 @@ public class PlayerDeath : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //spikes
+        //spikes and deathbox
         if (other.CompareTag("Environmental Hazard")){
             transform.position = checkpointPos.getLastCheckPointPos();
             playerTrail.Clear();
+            totalDeaths++;
         }
     }
-    
+    //we need to display the deathCount as text and we also need that text to properly be on screen
     
 }
