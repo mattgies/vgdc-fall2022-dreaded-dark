51b9e4a045e7ee8eff5885837f1ac69675b38bd6
deano4 <115955566+deano4@users.noreply.github.com>
2022-11-19 23:03:30 -0800

    Lv 2 Platform
    Includes a lv 2 platform. Got rid of camera lag because it didn't work with the falling, that code needs to be changed to accustom the falling elevation. Question difficulty to successfully pass the falling part. Tested with lights on and was possible, the questionable part is how possible it is with lights off. Haven't had any successful with lights turned off.

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -7,14 +7,7 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] GameObject thingToFollow;
     void LateUpdate()
     {
-        //camera will lag in the air on death
-        if (thingToFollow.transform.position.y < -10){
-            transform.position = new Vector3(thingToFollow.transform.position.x, -10, -10);
-        }
-        //otherwise it follows the player
-        else{
-            transform.position = thingToFollow.transform.position + new Vector3(0, 0, -10);   
-        }     
+            transform.position = thingToFollow.transform.position + new Vector3(0, 0, -10);     
     }
 
 
